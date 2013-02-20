2a28dd16544ca8a687e5e3025cb4260a91d4931b
Neem Serra <neemserra@gmail.com>
2013-02-19 21:01:12 -0600

    tests written first

--- src/main/java/neem/palindromes/Palindromes.java
+++ src/main/java/neem/palindromes/Palindromes.java
@@ -1,5 +1,36 @@
 package neem.palindromes;
 
-public class Palindromes {
+import java.util.List;
 
+public class Palindromes {
+	/**
+	 * A palindromic number reads the same both ways. 
+	 * The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99
+	 * What is the largest palindrome made from two 3-digit numbers?
+	 */
+	
+	int numberOfDigits;
+	
+	public Palindromes(int numberOfDigits) {
+		this.numberOfDigits = numberOfDigits;
+	}
+
+	public List<Integer> generatePotentialPalindromes(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Integer> isPalindrome(List<Integer> actualList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int largestPalindrome(List<Integer> actualPalindromeList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+
+	
 }
--- src/test/java/neem/palindromes/testPalindromes.java
+++ src/test/java/neem/palindromes/testPalindromes.java
@@ -2,14 +2,67 @@ package neem.palindromes;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class testPalindromes {
+	private Palindromes testObject;
+	
+	@Before
+	public void setUp() {
+		int numberOfDigits = 2;
+		testObject = new Palindromes(numberOfDigits);	
+	}
+	
+	@Test
+	public final void testGeneratePotentialPalindromes() {
+		//Assemble
+		List<Integer> expectedList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 15, 21, 24, 27, 20, 28, 32, 36, 25, 30, 35, 40, 45, 42, 48, 54, 49, 56, 63, 64, 72, 81);
+		//Act
+		List<Integer> actualList = new ArrayList<Integer>();
+		actualList = testObject.generatePotentialPalindromes(1);
+		
+		//Assert
+		assertEquals(expectedList, actualList);
+	}
+
 
 	@Test
-	public final void test() {
-		fail("Not yet implemented"); // TODO
+	public final void testIsPalindrome() {
+		//Assemble
+		List<Integer> expectedPalindromeList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		
+		//Act
+		List<Integer> actualList = new ArrayList<Integer>();
+		List<Integer> actualPalindromeList = new ArrayList<Integer>();
+		actualList = testObject.generatePotentialPalindromes(1);
+		actualPalindromeList = testObject.isPalindrome(actualList);
+		
+		//Assert
+		assertEquals(expectedPalindromeList, actualPalindromeList);
+	}
+	
+	
+	@Test
+	public final void testLargestPalindrome() {
+		//Assemble
+		int expectedLargestPalindrome = 9;
+		
+		//Act
+		List<Integer> actualList = new ArrayList<Integer>();
+		List<Integer> actualPalindromeList = new ArrayList<Integer>();
+		actualList = testObject.generatePotentialPalindromes(1);
+		actualPalindromeList = testObject.isPalindrome(actualList);
+		int actualLargestPalindrome = testObject.largestPalindrome(actualPalindromeList);
+		
+		//Assert
+		assertEquals(expectedLargestPalindrome, actualLargestPalindrome);
 	}
 
 }
