9c466fa99c214fdcb1a0ac951d4874fff9fe49cf
Neem Serra <neemserra@gmail.com>
2013-02-19 20:18:45 -0600

    initial commit

---
+++ src/main/java/neem/palindromes/Palindromes.java
@@ -0,0 +1,5 @@
+package neem.palindromes;
+
+public class Palindromes {
+
+}
---
+++ src/test/java/neem/palindromes/testPalindromes.java
@@ -0,0 +1,15 @@
+package neem.palindromes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class testPalindromes {
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
